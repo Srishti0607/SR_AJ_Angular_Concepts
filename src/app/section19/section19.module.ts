ad6d821e96f9fa8cf3bcef3de84d096df71666c9
srishti0607 <srishti2906@gmail.com>
2024-03-02 18:31:14 +0530

    Updated section 19

--- src/app/section19/section19.module.ts
+++ src/app/section19/section19.module.ts
@@ -2,16 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Section19RoutingModule } from './section19.routing';
 import { ExportToFileFormatsDemoComponent } from './components/export-to-file-formats-demo/export-to-file-formats-demo.component';
+import { CustomdemoService } from '../services/customdemo.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ExportAsModule, ExportAsService } from 'ngx-export-as';
 
 
 
 @NgModule({
   declarations: [
-  // ExportToFileFormatsDemoComponent
+  ExportToFileFormatsDemoComponent
   ],
   imports: [
     CommonModule,
-    Section19RoutingModule
+    Section19RoutingModule,
+    HttpClientModule,
+    ExportAsModule
+  ],
+  providers: [
+    CustomdemoService,
+    ExportAsService
   ]
 })
 export class Section19Module { }
