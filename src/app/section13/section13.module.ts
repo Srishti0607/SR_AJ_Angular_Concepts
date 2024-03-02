d714ea1ccf06f511c18e6dbc711858c30cc16f5f
srishti.rathi <srishti.rathi@ril.com>
2024-03-02 15:04:41 +0530

    added section13

---
+++ src/app/section13/section13.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Section13RoutingModule } from './section13.routing';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    Section13RoutingModule
+  ]
+})
+export class Section13Module { }
